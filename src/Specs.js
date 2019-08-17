90b8623db367b4517d6132add877a88e97f23851
Lia <lia.tabilo@gmail.com>
2019-08-17 20:43:15 +0100

    Initial commit

---
+++ src/Specs.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <header className="header .d-sm-flex">
+            Na'dia
+            <br />
+            Correia Gonc,alves
+          </header>
+        </h1>
+        <h2 />
+        <h3 />
+      </div>
+    );
+  }
+}
+
+export default Main;
