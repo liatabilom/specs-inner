0476ca107ae883edc246b85d12617eec334a9bb2
Lia <lia.tabilo@gmail.com>
2019-08-22 17:55:42 +0100

    Update button for accordion

---
+++ src/Pieces.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Pieces.css";
+
+class Technical extends Component {
+  render() {
+    return (
+      <div className="container technicalContainer" style={{ width: 1140 }} />
+    );
+  }
+}
+
+export default Technical;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import useCollapse from 'react-collapsed';
+import React, { useState } from "react";
+import useCollapse from "react-collapsed";
 import Card13 from "./images/Card13.png";
 import Card12 from "./images/Card12.png";
 import Card11 from "./images/Card11.png";
@@ -11,32 +11,37 @@ import "./Sidebar.css";
 
 function Sidebar() {
   const [isOpen, setOpen] = useState(false);
-  const {getCollapseProps, getToggleProps} = useCollapse({expandStyles:{transitionProperty:"width"}}, {isOpen});
+  const { getCollapseProps, getToggleProps } = useCollapse(
+    { expandStyles: { transitionProperty: "width" } },
+    { isOpen }
+  );
 
   return (
     <React.Fragment>
-      <button className="btn btn-dark-outline btnAccordion"
+      <button
+        className="btn btn-outline-dark btnAccordion"
         {...getToggleProps({
-          onClick: () => setOpen(oldOpen => !oldOpen),
+          onClick: () => setOpen(oldOpen => !oldOpen)
         })}
       >
-        {isOpen ? '<' : '|'}
+        {isOpen ? "<" : "|"}
       </button>
-      <section {...getCollapseProps()}><div>
-        <div className="containerAccordion d-inline">
-    <div className="gallery-wrap">
-    <div className="item item-1 .active"></div>
-    <div className="item item-2"></div>
-    <div className="item item-3"></div>
-    <div className="item item-4"></div>
-    <div className="item item-5"></div>
-    <div className="item item-6"></div>
-  </div>
- </div>
-      </div></section>
+      <section {...getCollapseProps()}>
+        <div>
+          <div className="containerAccordion d-inline">
+            <div className="gallery-wrap">
+              <div className="item item-1 .active" />
+              <div className="item item-2" />
+              <div className="item item-3" />
+              <div className="item item-4" />
+              <div className="item item-5" />
+              <div className="item item-6" />
+            </div>
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   );
 }
-
 
 export default Sidebar;
