7f98d6cf6f996bb5b527a54f1aac174d14f0eec8
Lia <lia.tabilo@gmail.com>
2019-11-17 11:39:23 +0000

    Adjust subheader text

--- src/components/technical/SubHeader.js
+++ src/components/technical/SubHeader.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import './SubHeader.css';
+import React, { Component } from "react";
+import "./SubHeader.css";
 
 class SubHeader extends Component {
-	render() {
-		return (
-			<div>
-				<div className="row subHeaderContainer" style={{ margin: 0 }}>
-					<div className="card-body col-4 p-0 subHeader">SPEC PACKS</div>
-					<div className="card-body col-4 p-0 subHeader">ALL PIECES DESIGNED AND DRAWN</div>
-					<div className="card-body col-4 p-0 subHeader rightHeader">AW18</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div className="row subHeaderContainer" style={{ margin: 0 }}>
+          <div className="card-body col-4 p-0 subHeader">SPEC PACKS</div>
+          <div className="card-body col-4 p-0 subHeader">
+            ALL PIECES DESIGNED AND DRAWN
+          </div>
+          <div className="card-body col-4 p-0 subHeader rightHeader">AW18</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SubHeader;
